634f844aad4e8704c4098ac4b4d03670de529aae
Yogi Prayogi <kulsemyogi@gmail.com>
2020-11-04 09:00:33 +0700

    first commit

---
+++ Praktikum/praktikum1/kenalin.php
@@ -0,0 +1,10 @@
+<?php
+class kenalin extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Yogi Prayogi<br>
+                saya tinggal di Brebes<br>
+                saya suka olahraga futsal";
+    }
+
+}
